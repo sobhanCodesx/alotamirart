016c854cca9b30dafb99685109f84a8bdfdb7f2f
sobhanCodesx <sobhankhii22@gmail.com>
2026-09-21 15:39:09 +0330

    Cache bust public stylesheet after deploys

--- them/app/layout/heading.php
+++ them/app/layout/heading.php
@@ -1,4 +1,7 @@
+<?php
+$siteCssVersion = @filemtime(BASE_PATH . '/public/src/css/site.css') ?: '1';
+?>
 <link rel="stylesheet" href="<?= assets('public/src/css/bootstrap.rtl.min.css') ?>">
-<link rel="stylesheet" href="<?= assets('public/src/css/site.css') ?>">
+<link rel="stylesheet" href="<?= assets('public/src/css/site.css') ?>?v=<?= urlencode((string)$siteCssVersion) ?>">
 <link rel="icon" type="image/x-icon" href="<?= assets(isset($dataSeo['logo']) ? $dataSeo['logo'] : 'public/src/img/logo.png') ?>">
 <meta name="theme-color" content="#172554">
