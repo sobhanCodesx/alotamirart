06215ec754ebfee94a6b3b5ea4c30ff3a54dd000
sobhanCodesx <sobhankhii22@gmail.com>
2026-09-21 14:01:50 +0330

    Route legacy PHP endpoints through front controller locally

--- dev-router.php
+++ dev-router.php
@@ -1,7 +1,19 @@
 <?php
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$file = __DIR__ . $path;
 
+$legacyFrontControllerEndpoints = [
+    '/service-city.php',
+    '/show-city.php',
+    '/city-services.php',
+    '/service-washing-machine.php',
+];
+
+if (in_array($path, $legacyFrontControllerEndpoints, true)) {
+    require __DIR__ . '/index.php';
+    return true;
+}
+
+$file = __DIR__ . $path;
 if ($path !== '/' && is_file($file)) {
     return false;
 }
