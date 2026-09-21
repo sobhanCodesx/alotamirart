be5fde7bd3ef50105386eb1a47a0a07af4a83869
sobhanCodesx <sobhankhii22@gmail.com>
2026-09-21 15:37:08 +0330

    Normalize text in sidebar cards

--- them/app/layout/sidbar.php
+++ them/app/layout/sidbar.php
@@ -5,10 +5,10 @@
       <?php if (!empty($side_post) && is_array($side_post)): ?>
         <?php foreach ($side_post as $pu): ?>
           <a class="sidebar-item" href="<?= assets('post/' . (int)$pu['id']) ?>">
-            <img src="<?= assets($pu['img']) ?>" alt="<?= htmlspecialchars(isset($pu['title']) ? $pu['title'] : '', ENT_QUOTES, 'UTF-8') ?>" width="72" height="64" loading="lazy" decoding="async">
+            <img src="<?= assets($pu['img']) ?>" alt="<?= htmlspecialchars(clean_display_text(isset($pu['title']) ? $pu['title'] : ''), ENT_QUOTES, 'UTF-8') ?>" width="72" height="64" loading="lazy" decoding="async">
             <span>
-              <strong><?= htmlspecialchars(limit_words(strip_tags(isset($pu['title']) ? $pu['title'] : ''), 7), ENT_QUOTES, 'UTF-8') ?></strong>
-              <span><?= htmlspecialchars(limit_words(strip_tags(isset($pu['content']) ? $pu['content'] : ''), 10), ENT_QUOTES, 'UTF-8') ?></span>
+              <strong><?= htmlspecialchars(excerpt_text(isset($pu['title']) ? $pu['title'] : '', 7), ENT_QUOTES, 'UTF-8') ?></strong>
+              <span><?= htmlspecialchars(excerpt_text(isset($pu['content']) ? $pu['content'] : '', 10), ENT_QUOTES, 'UTF-8') ?></span>
             </span>
           </a>
         <?php endforeach; ?>
@@ -22,10 +22,10 @@
       <?php if (!empty($side_brand) && is_array($side_brand)): ?>
         <?php foreach ($side_brand as $pu): ?>
           <a class="sidebar-item" href="<?= assets((isset($pu['slug']) ? $pu['slug'] : '') . '/' . (int)$pu['id']) ?>">
-            <img src="<?= assets($pu['img']) ?>" alt="<?= htmlspecialchars(isset($pu['title']) ? $pu['title'] : '', ENT_QUOTES, 'UTF-8') ?>" width="72" height="64" loading="lazy" decoding="async">
+            <img src="<?= assets($pu['img']) ?>" alt="<?= htmlspecialchars(clean_display_text(isset($pu['title']) ? $pu['title'] : ''), ENT_QUOTES, 'UTF-8') ?>" width="72" height="64" loading="lazy" decoding="async">
             <span>
-              <strong><?= htmlspecialchars(limit_words(strip_tags(isset($pu['title']) ? $pu['title'] : ''), 7), ENT_QUOTES, 'UTF-8') ?></strong>
-              <span><?= htmlspecialchars(limit_words(strip_tags(isset($pu['content']) ? $pu['content'] : ''), 10), ENT_QUOTES, 'UTF-8') ?></span>
+              <strong><?= htmlspecialchars(excerpt_text(isset($pu['title']) ? $pu['title'] : '', 7), ENT_QUOTES, 'UTF-8') ?></strong>
+              <span><?= htmlspecialchars(excerpt_text(isset($pu['content']) ? $pu['content'] : '', 10), ENT_QUOTES, 'UTF-8') ?></span>
             </span>
           </a>
         <?php endforeach; ?>
