55adc94c4059e326c9ee602ab4b5ccd74990e50a
sobhanCodesx <sobhankhii22@gmail.com>
2026-09-21 15:02:50 +0330

    Keep legacy city URL compatible

--- show-city.php
+++ show-city.php
@@ -1,1 +1,7 @@
-<?php require BASE_PATH . '/them/app/cities/show.php'; ?>
+<?php
+if (!defined('BASE_PATH')) {
+    $slug = isset($_GET['city']) ? preg_replace('/[^a-zA-Z0-9-]/', '', (string)$_GET['city']) : '';
+    header('Location: /services-in-' . ($slug !== '' ? $slug : 'tehran'), true, 301);
+    exit;
+}
+require BASE_PATH . '/them/app/cities/show.php';
