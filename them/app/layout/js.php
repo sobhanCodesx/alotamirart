d1462153e30d9e0951a832c7ca4702fec6ac7261
sobhanCodesx <sobhankhii22@gmail.com>
2026-09-21 15:39:11 +0330

    Cache bust public menu script after deploys

--- them/app/layout/js.php
+++ them/app/layout/js.php
@@ -1,2 +1,5 @@
+<?php
+$siteJsVersion = @filemtime(BASE_PATH . '/public/src/js/site.js') ?: '1';
+?>
 <script src="<?= assets('public/src/js/bootstrap.min.js') ?>" defer></script>
-<script src="<?= assets('public/src/js/site.js') ?>" defer></script>
+<script src="<?= assets('public/src/js/site.js') ?>?v=<?= urlencode((string)$siteJsVersion) ?>" defer></script>
